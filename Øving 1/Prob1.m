222bbf2c9226ad46c83f16746aa11b53af9f8a8f
Magnus Dyre-Moe <magnus.dyremoe@gmail.com>
2020-09-04 12:48:30 +0200

    finished with assignment 1

--- Øving 1/Prob1.m
+++ Øving 1/Prob1.m
@@ -5,7 +5,7 @@ R_33 = 2.0;
 I_g = m*R_33^2 * eye(3);
 tau = sym('t', [3,1]);
 
-%State
+%States
 q = sym('q', [4,1]);
 e = q(2:4);
 w = sym('w', [3,1]);
@@ -22,10 +22,9 @@ S_Iw = [0 -Iw(3) Iw(2) ;
 w_dot = inv(I_g) * (tau + S_Iw * w);
 
 %Linearization
-df1_dx = jacobian(q_dot, x);
-df2_dx = jacobian(w_dot, x);
+A = [jacobian(q_dot, x);
+    jacobian(w_dot, x)];
 
-A = [df1_dx; df2_dx];
 B = [jacobian(q_dot, tau); 
     jacobian(w_dot, tau)];
 
@@ -40,8 +39,9 @@ K = [k_p*eye(3) K_d];
 tau = -K*x;
 
 %System stability with state feedback given by eig(A-BK)
-
+%around equilibrium
 mat = [eye(3)*0, eye(3)*0.5;
        -eye(3)*k_p/720, -eye(3)*k_d/720]
 eig(mat)
 
+check_skew = Smtrx(e)*e % = 0 (
